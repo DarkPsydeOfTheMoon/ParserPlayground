d13c2d81759024000aeb99cab5ed1e28446c2625
DarkPsydeOfTheMoon <avalanchebreakdown@gmail.com>
2024-09-24 22:47:35 -0700

    Added extra entry in field BGM script.

--- Scripts/AddFieldBGMTableEntries.py
+++ Scripts/AddFieldBGMTableEntries.py
@@ -14,12 +14,20 @@ def main():
 	table1.read(tablePath1, filename=tableName)
 
 	# add an entry (at the BEGINNING of the table)
-	newEntry = FtdEntryTypes.FLDBGMCND()
-	newEntry.FieldMajorId = 15
-	newEntry.FieldMinorId = 1
-	newEntry.FieldType = 0
-	newEntry.BgmCueId = 41
-	table1.Entries[0].Entries.insert(0, newEntry)
+	newEntry1 = FtdEntryTypes.FLDBGMCND()
+	newEntry1.FieldMajorId = 15
+	newEntry1.FieldMinorId = 1
+	newEntry1.FieldType = 0
+	newEntry1.BgmCueId = 41
+	table1.Entries[0].Entries.insert(0, newEntry1)
+
+	# ...and another one just for coverage lol
+	newEntry2 = FtdEntryTypes.FLDBGMCND()
+	newEntry2.FieldMajorId = 1
+	newEntry2.FieldMinorId = 6
+	newEntry2.FieldType = 0
+	newEntry2.BgmCueId = 41
+	table1.Entries[0].Entries.insert(0, newEntry2)
 
 	# save the results
 	tablePath2 = "Scripts/Assets/MODDED_FLDBGMCND.FTD"
