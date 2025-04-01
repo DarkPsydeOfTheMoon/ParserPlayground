8cc5e5734ff02cd3e76e77a44c4bb309b4de8f0b
DarkPsydeOfTheMoon <avalanchebreakdown@gmail.com>
2025-04-01 09:25:05 -0700

    Further edits to FLYPTYTALKCAR, lol.

--- Scripts/EditCarTalkEntry.py
+++ Scripts/EditCarTalkEntry.py
@@ -13,8 +13,25 @@ def main():
 	table1 = Table()
 	table1.read(tablePath1, filename=tableName)
 
+	# remove mona asking ann on an evening stroll
 	print(table1.Entries[3].Entries[29].stringify())
 	table1.Entries[3].Entries[29].SupportMsgIndex = 65535
+
+	# remove talk about ann's t&a lmao
+	print(table1.Entries[3].Entries[68].stringify())
+	print(table1.Entries[4].Entries[0].stringify())
+	print(table1.Entries[18].Entries[15].stringify())
+	table1.Entries[3].Entries[68].SupportMsgIndex = 65535
+	table1.Entries[4].Entries[0].SupportMsgIndex = 65535
+	table1.Entries[18].Entries[15].SupportMsgIndex = 65535
+
+	# remove more talk about ann's t&a...
+	print(table1.Entries[3].Entries[75].stringify())
+	print(table1.Entries[4].Entries[18].stringify())
+	print(table1.Entries[19].Entries[10].stringify())
+	table1.Entries[3].Entries[75].SupportMsgIndex = 65535
+	table1.Entries[4].Entries[18].SupportMsgIndex = 65535
+	table1.Entries[19].Entries[10].SupportMsgIndex = 65535
 
 	# save the results
 	tablePath2 = "Scripts/Assets/MODDED_FLDPTYTALKCAR.PTD"
@@ -28,7 +45,14 @@ def main():
 		assert table1.Entries[i].EntryCount == table2.Entries[i].EntryCount
 
 	# show the results!
+	print("##########")
 	print(table1.Entries[3].Entries[29].stringify())
+	print(table1.Entries[3].Entries[68].stringify())
+	print(table1.Entries[4].Entries[0].stringify())
+	print(table1.Entries[18].Entries[15].stringify())
+	print(table1.Entries[3].Entries[75].stringify())
+	print(table1.Entries[4].Entries[18].stringify())
+	print(table1.Entries[19].Entries[10].stringify())
 
 
 if __name__ == "__main__":
